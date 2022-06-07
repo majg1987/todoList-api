5c3b57cadad5a5487c2cae46c20dd501f7f28e56
majg1987 <majg1987@hotmail.com>
2022-06-07 13:31:23 +0000

    proyecto terminado v1

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+
+const Card = ({ listaTareas, setListaTareas, update }) => {
+	const eliminar = (element) => {
+		let filter = listaTareas.filter((tarea) => {
+			return tarea.label !== element.label;
+		});
+		setListaTareas(filter);
+	};
+
+	useEffect(() => {
+		console.log(listaTareas);
+		if (listaTareas.length > 0) {
+			update(listaTareas);
+		}
+	}, [listaTareas]);
+
+	return (
+		<>
+			<div className="card tarjeta mt-3 mx-auto fw-bolder">
+				<div className="card-body border border-info">
+					<ul className="list-group-item border border-warning">
+						{listaTareas.length === 0 ? (
+							<p className="text-info text-center">
+								Añade Tareas a la Lista
+							</p>
+						) : (
+							listaTareas.map((element, index) => {
+								return (
+									<li
+										key={index}
+										className="d-flex justify-content-between text-info p-2">
+										{element.label}
+										<button
+											onClick={() => eliminar(element)}
+											className="bg-danger rounded-circle text-white border-0">
+											<i className="far fa-trash-alt"></i>
+										</button>
+									</li>
+								);
+							})
+						)}
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,94 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import Card from "./Card.jsx";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [alert, setAlert] = useState(false);
+	const [listaTareas, setListaTareas] = useState([]);
+
+	const añadir = (e) => {
+		if (e.key === "Enter") {
+			if (tarea === "") {
+				setAlert(true);
+			}
+			if (tarea.length > 0) {
+				setAlert(false);
+				setListaTareas([...listaTareas, { label: tarea, done: false }]);
+				setTarea("");
+			}
+		}
+	};
+
+	const limpiar = () => {
+		setListaTareas([]);
+	};
+
+	const update = (listaTareas) => {
+		const requestOptionsInsertarTarea = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(listaTareas),
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/miguelangeljurado",
+			requestOptionsInsertarTarea
+		).then((response) => response.json());
+	};
+
+	useEffect(() => {
+		const getLista = () => {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/miguelangeljurado"
+			)
+				.then((response) => response.json())
+				.then((data) => setListaTareas(data));
+		};
+		getLista();
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center text-warning fw-bolder ">
+				Lista de Tareas
+			</h1>
+			<div className="input-group flex-nowrap">
+				<input
+					type="text"
+					className="form-control mx-auto border border-info text-info fw-bolder"
+					placeholder="Introduce tu Tarea"
+					aria-label="Username"
+					aria-describedby="addon-wrapping"
+					onChange={(e) => setTarea(e.target.value)}
+					onKeyDown={añadir}
+					value={tarea}
+				/>
+			</div>
+			<Card
+				listaTareas={listaTareas}
+				setListaTareas={setListaTareas}
+				update={update}
+			/>
+			<div
+				className={`${
+					alert
+						? "alert alert-danger mt-4 mx-auto alerta text-center"
+						: ""
+				}`}
+				role="alert">
+				{`${alert ? "No puedes insertar una tarea vacia" : ""}`}
+			</div>
+			<div
+				className=" text-center mt-3"
+				role="group"
+				aria-label="Basic mixed styles example">
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={limpiar}>
+					Limpiar
+				</button>
+			</div>
 		</div>
 	);
 };
